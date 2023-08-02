536f8495d195e9f35aa94e0e1f171cad7c8f1042
Albin Heimerson <albin.heimerson@control.lth.se>
2023-08-02 09:30:33 +0200

    fix csv saving

--- plotting/save_to_csv.jl
+++ plotting/save_to_csv.jl
@@ -3,24 +3,28 @@ Pkg.activate(@__DIR__)
 
 include(joinpath(@__DIR__, "readdata.jl"))
 
-exp_path = joinpath(homedir(), "servicemesh_results", "double_new", "random_initial")
+# Changing experiment
+env_name = "double_new"
+n_ms = 4
+n_workloads = 3
+
+tag = "50days"
+
+exp_path = joinpath(homedir(), "servicemesh_results", env_name, tag)
 
 data_paths = read_all(exp_path)
 
 datas = [loaddata(data_paths[k]) for k in keys(data_paths)]
 data_names = ["k8s" "oracle" "rl" "simple"]
 
-n_ms = 4
-n_workloads = 3
+ts = gettime(datas[1])
 
 smoothing_window = 1000
 downsampling = 1000
-days = 100
+days = 50
 final_sample = days * 24 * 60 # We sample every minute
 sample_range = 1:downsampling:final_sample
-
-ts = gettime(datas[1])
-sample_range = 1:downsampling:length(ts)
+#sample_range = 1:downsampling:length(ts)
 
 t = ts[sample_range]
 
@@ -49,6 +53,9 @@ end
 
 writedlm(joinpath(exp_path, "plot_data.csv"), data, ',')
 
+
+
+# Testing
 using Plots
 plot(t, reward_data, label=data_names)
 plot(t, arrivals_data)
@@ -60,6 +67,5 @@ p3 = plot(t, scale[3], label=data_names)
 p4 = plot(t, scale[4], label=data_names)
 plot(p1, p2, p3, p4, layout=4)
 
-
-
-#time,k8s_scale1,oracle_scale1,rl_scale1,simple_scale1,k8s_util1,oracle_util1,rl_util1,simple_util1,k8s_scale2,oracle_scale2,rl_scale2,simple_scale2,k8s_util2,oracle_util2,rl_util2,simple_util2,k8s_scale3,oracle_scale3,rl_scale3,simple_scale3,k8s_util3,oracle_util3,rl_util3,simple_util3,k8s_scale4,oracle_scale4,rl_scale4,simple_scale4,k8s_util4,oracle_util4,rl_util4,simple_util4,arrivals1_mean,arrivals2_mean,arrivals1,arrivals2,k8s_missed,oracle_missed,rl_missed,simple_missed,k8s_reward,oracle_reward,rl_reward,simple_reward
+# expected colnames
+# time,k8s_scale1,oracle_scale1,rl_scale1,simple_scale1,k8s_util1,oracle_util1,rl_util1,simple_util1,k8s_scale2,oracle_scale2,rl_scale2,simple_scale2,k8s_util2,oracle_util2,rl_util2,simple_util2,k8s_scale3,oracle_scale3,rl_scale3,simple_scale3,k8s_util3,oracle_util3,rl_util3,simple_util3,k8s_scale4,oracle_scale4,rl_scale4,simple_scale4,k8s_util4,oracle_util4,rl_util4,simple_util4,arrivals1_mean,arrivals2_mean,arrivals1,arrivals2,k8s_missed,oracle_missed,rl_missed,simple_missed,k8s_reward,oracle_reward,rl_reward,simple_reward
